f3836dd1aac27aa834f6ef98cb89fc44f2175db1
Sebi Cozma <sebi.sam.cozma@gmail.com>
2025-05-18 13:56:49 +0300

    top testbench update

--- tb_top.v
+++ tb_top.v
@@ -1,10 +1,12 @@
 `timescale 1ns/1ns
 module tb_top();
     reg clk, rst;
+    reg bat_size, en, en2;
+    reg [1:0] mode;
     wire [7:0] RED, GRN, BLU;
-    wire HSYNC, VSYNC;
+    wire HSYNC, VSYNC, px_clk, blank;
 
-    top top(clk, rst, RED, GRN, BLU, HSYNC, VSYNC);
+    top top(clk, rst, bat_size, en, en2, mode, RED, GRN, BLU, HSYNC, VSYNC, px_clk, blank);
 
     initial begin
         clk = 0;
@@ -14,6 +16,10 @@ module tb_top();
     initial begin
         rst = 1'b1;
 
-        #20 rst = 1'b0;      
+        #20 rst = 1'b0;
+        #100 bat_size = 1'b0;
+        en = 1'b1;
+        en2 = 1'b1;
+        mode = 2'b00;      
     end
 endmodule
