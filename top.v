05153507bb1446d538c3c07444b4c036e3d0edd7
Sebi Cozma <sebi.sam.cozma@gmail.com>
2025-05-18 18:50:16 +0300

    divider line fix

--- top.v
+++ top.v
@@ -8,7 +8,7 @@ module top(input clk, rst, bat_size, en, en2, input[1:0] mode, output [9:0] RED,
     assign blank = 1'b1;
 
     demo_players demo_players(clk, rst, en, p1_y, p2_y);
-    demo_ball demo_ball(clk, rst, 1, bx, by);
+    demo_ball demo_ball(clk, rst, en, bx, by);
     demo_score demo_score(clk, rst, en2, score1, score2);
     clk_divider clk_divider(clk, rst, px_clk);
     video_encoder video_encoder(clk, rst, bat_size, mode, score1, score2, p1_y, p2_y, bx, by, x, y, px_data_out);
