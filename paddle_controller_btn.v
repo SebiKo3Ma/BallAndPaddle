96ee163b9880530878b85dc8f905373f841eac0d
Sebi Cozma <sebi.sam.cozma@gmail.com>
2025-05-20 16:02:21 +0300

    paddle temp controller

---
+++ paddle_controller_btn.v
@@ -0,0 +1,55 @@
+module paddle_controller_btn(input clk, rst, p1p, p1m, p2p, p2m, bat_size, output[10:0] p1_y, p2_y);
+    reg [16:0] counter_ff, counter_nxt;
+    reg [10:0] p1_ff, p1_nxt, p2_ff, p2_nxt;
+
+    assign p1_y = p1_ff;
+    assign p2_y = p2_ff;
+
+    always @* begin
+        p1_nxt = p1_ff;
+        p2_nxt = p2_ff;
+        counter_nxt = counter_ff + 1;
+
+        if(!counter_ff) begin
+            if(p1p) begin
+                p1_nxt = p1_ff + 1;
+            end
+
+            if(p1m) begin
+                p1_nxt = p1_ff - 1;
+            end
+
+            if(p2p) begin
+                p2_nxt = p2_ff + 1;
+            end
+
+            if(p2m) begin
+                p2_nxt = p2_ff - 1;
+            end
+        end
+
+        if(bat_size) begin
+            if(p1_ff < 11'd40)  p1_nxt = 11'd40;
+            if(p1_ff > 11'd440) p1_nxt = 11'd440;
+            if(p2_ff < 11'd40)  p2_nxt = 11'd40;
+            if(p2_ff > 11'd440) p2_nxt = 11'd440;
+        end else begin
+            if(p1_ff < 11'd50)  p1_nxt = 11'd50;
+            if(p1_ff > 11'd430) p1_nxt = 11'd430;
+            if(p2_ff < 11'd50)  p2_nxt = 11'd50;
+            if(p2_ff > 11'd430) p2_nxt = 11'd430;
+        end
+    end
+
+    always @(posedge clk or posedge rst) begin
+        if(rst) begin
+            p1_ff <= 11'd240;
+            p2_ff <= 11'd240;
+            counter_ff <= 17'd1;
+        end else begin
+            p1_ff <= p1_nxt;
+            p2_ff <= p2_nxt;
+            counter_ff <= counter_nxt;
+        end
+    end
+endmodule
--- top.v
+++ top.v
@@ -1,17 +1,23 @@
-module top(input clk, rst, ball_speed, angle, bat_size, input[1:0] mode, output [9:0] RED, GRN, BLU, output HSYNC, VSYNC, px_clk, blank);
+module top(input clk, rst, ball_speed, angle, bat_size, p1p, p1m, p2p, p2m, input[1:0] mode, output [9:0] RED, GRN, BLU, output HSYNC, VSYNC, px_clk, blank);
     wire [10:0] x, y, p1_y, p2_y, bx, by;
     wire px_data_out;
     wire [29:0] px_data_in;
     wire [4:0] score1, score2;
+    wire wp1p, wp1m, wp2p, wp2m;
 
     assign px_data_in = {30{px_data_out}};
     assign blank = 1'b1;
 
-    // demo_players demo_players(clk, rst, en, p1_y, p2_y);
+    // demo_players demo_players(clk, rst, 1'b1, p1_y, p2_y);
     // demo_ball demo_ball(clk, rst, en, bx, by);
     // demo_score demo_score(clk, rst, en2, score1, score2);
+    debouncer d1(px_clk, rst, p1p, wp1p);
+    debouncer d2(px_clk, rst, p1m, wp1m);
+    debouncer d3(px_clk, rst, p2p, wp2p);
+    debouncer d4(px_clk, rst, p2m, wp2m);
+    paddle_controller_btn pctrl(clk, rst, wp1p, wp1m, wp2p, wp2m, bat_size, p1_y, p2_y);
     clk_divider clk_divider(clk, rst, px_clk);
-    game_controller game_controller(clk, rst, 11'd162, 11'd318, mode, ball_speed, 1'b0, angle, bat_size, 1'b0, score1, score2, p1_y, p2_y, bx, by);
+    game_controller game_controller(clk, rst, p1_y, p2_y, mode, ball_speed, 1'b0, angle, bat_size, 1'b0, score1, score2, p1_y, p2_y, bx, by);
     video_encoder video_encoder(clk, rst, bat_size, mode, score1, score2, p1_y, p2_y, bx, by, x, y, px_data_out);
     vga_controller vga_controller(px_clk, rst, px_data_in, x, y, RED, GRN, BLU, HSYNC, VSYNC);
 endmodule
